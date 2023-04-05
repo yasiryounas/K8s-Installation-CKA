1bb7923a0e6bf3f973449360ba2daf6a632e40fa
yasiryounas <yasiryounas100@gmail.com>
2023-04-06 00:02:42 +0500

    Initialization first code

---
+++ install-containerd.sh
@@ -0,0 +1,26 @@
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+sudo yum update
+sudo yum install containerd -y
+
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+service containerd status
---
+++ verification.sh
@@ -0,0 +1,6 @@
+lsmod | grep br_netfilter
+lsmod | grep overlay
+
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
+
+#apt-cache/yum madison kubeadm
