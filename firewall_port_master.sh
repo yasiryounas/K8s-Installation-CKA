d72772881d83cd80efeae2f060db7dc299db6beb
yasiryounas <yasiryounas100@gmail.com>
2023-07-04 16:03:54 +0500

    Update firewall_port_master.sh

--- firewall_port_master.sh
+++ firewall_port_master.sh
@@ -11,4 +11,5 @@ firewall-cmd --permanent --add-port=10259/tcp --zone=internal
 firewall-cmd --permanent --add-port=10257/tcp --zone=internal
 firewall-cmd --permanent --add-port=6783/tcp --zone=internal
 firewall-cmd --permanent --add-service=dns --zone=public
+firewall-cmd --add-masquerade --permanent
 firewall-cmd --reload
