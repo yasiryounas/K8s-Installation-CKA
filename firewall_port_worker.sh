ac1fac3dd4d220301b0362b655c8fd1e7a656379
yasiryounas <yasiryounas100@gmail.com>
2023-07-04 16:04:22 +0500

    Update firewall_port_worker.sh

--- firewall_port_worker.sh
+++ firewall_port_worker.sh
@@ -6,4 +6,5 @@ firewall-cmd --permanent --add-port=10250/tcp --zone=internal
 firewall-cmd --permanent --add-port=30000-32767/tcp --zone=internal
 firewall-cmd --permanent --add-port=30000-32767/tcp --zone=external
 firewall-cmd --permanent --add-port=6783/tcp --zone=internal
+firewall-cmd --add-masquerade --permanent
 firewall-cmd --reload
